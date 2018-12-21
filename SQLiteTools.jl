f0889cb4cfc555bc3963f478dc4ed70456c31195
Matt H <lawless-m@users.noreply.github.com>
2018-12-21 12:53:32 +0000

    column_n was silly

--- SQLiteTools.jl
+++ SQLiteTools.jl
@@ -6,7 +6,7 @@ using DataArrays
 inserts = Dict()
 updates = Dict()
 
-export bind!, exebind!, truncate!, column_n, table_by, select_all, insert!, missInt, int2date, int2time
+export bind!, exebind!, truncate!, table_by, select_all, insert!, missInt, int2date, int2time
 
 missInt(v) = typeof(v) == Missings.Missing ? 0 : convert(Int, v)
 int2date(dv) = Date(Dates.UTD(missInt(dv)))
@@ -29,10 +29,7 @@ end
 
 truncate!(db, table::String) = SQLite.query(db, "DELETE FROM $table")
 
-column_n(db, table::String, n::Integer) = SQLite.query(db, "SELECT * from $table")[1:end, n]
-
 table_by(db, table::String, orderby::String) = SQLite.query(db, "select * from $table Order By $orderby")	
-
 
 bind!(ins::String, coln::Int, val) = SQLite.bind!(inserts[ins], coln, val)
 
